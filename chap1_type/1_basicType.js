1d5c1512ba476c285faf96786d7be38f4ea8e5c3
JUNGEEYOU <junge2u@naver.com>
2017-12-22 18:09:45 +0900

    기본 타입

---
+++ chap1_type/1_basicType.js
@@ -0,0 +1,25 @@
+//1. 자바스크립트 데이터 타입과 연산자 
+//자바스크립트 타입: 1) 기본 타입:숫자, 문자열, 불린값,undefied, null
+//2) 참조타입: 객체> 배열, 함수, 정규표현식 
+
+
+//3.1 자바스크립트의 기본타입: 그 자체가 하나의 값을 나타냄 
+// 1) 숫자 타입 
+var intNum =10;
+var floatNum = 0.1;
+
+//2) 문자열 타입 
+var singleQuoteStr ="single quote string";
+var dobuleQuoteStr ="double quote string";
+var singleChar ="a";
+
+//3)불린 타입
+var boolVar = true;
+
+//4) undefined 
+var emptyVar;
+
+//5) null타입 
+var nullVar =null;
+
+console.log(typeof intNum, typeof floatNum, typeof singleQuoteStr, typeof dobuleQuoteStr, typeof singleQuoteStr, typeof boolVar, typeof emptyVar,typeof nullVar);
